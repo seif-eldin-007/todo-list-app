dec625da1cc6d74594ee7f8cba0c94b18d099e49
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-06-16 23:58:36 +0300

    fix: used mongoose built-in timestapms instead of createdAt, updatedAt

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,29 +1,26 @@
 import mongoose from "mongoose";
 
-const TodoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const TodoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["todo", "in-progress", "done"],
+      default: "todo",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["todo", "in-progress", "done"],
-    default: "todo",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Todo = mongoose.model("Todo", TodoSchema);
 
