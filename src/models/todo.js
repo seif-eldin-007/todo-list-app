e46521dcecffc9bf1f563cba8ee42600705577e7
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-06-16 23:51:09 +0300

    feat: created the todo mongoose model

---
+++ src/models/todo.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["todo", "in-progress", "done"],
+    default: "todo",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+export default Todo;
